21531e666962c681f44f0f0e91290e7f1f74fcc1
rak1312 <rak123@hotmail.com>
2024-02-14 14:42:29 +0530

    Update cancel_open_orders.py

--- cancel_open_orders.py
+++ cancel_open_orders.py
@@ -15,44 +15,7 @@ def filter_custom(obj, *args):
 
 
 # Reading cancel order criteria from file ordercancel.json
-'''
-Possible Filter Values
-{
-        "order_id": "202402132400013696",
-        "exchange_order_id": "1400000048534549",
-        "exchange_code": "NFO",
-        "stock_code": "CNXBAN",
-        "product_type": "Options",
-        "action": "Sell",
-        "order_type": "Limit",
-        "stoploss": "0",
-        "quantity": "900",
-        "price": "3.85",
-        "validity": "Day",
-        "disclosed_quantity": "0",
-        "expiry_date": "21-Feb-2024",
-        "right": "Call",
-        "strike_price": 53000.0,
-        "average_price": "0",
-        "cancelled_quantity": "900",
-        "pending_quantity": "900",
-        "status": "Cancelled",
-        "user_remark": null,
-        "order_datetime": "13-Feb-2024 11:39:09",
-        "parent_order_id": "",
-        "modification_number": null,
-        "exchange_acknowledgement_date": null,
-        "SLTP_price": null,
-        "exchange_acknowledge_number": null,
-        "initial_limit": null,
-        "intial_sltp": null,
-        "LTP": null,
-        "limit_offset": null,
-        "mbc_flag": null,
-        "cutoff_price": null,
-        "validity_date": null
-    }
-'''
+
 f = open ('./ordercancel.json', "r")
 filter_cr = json.loads(f.read())
 f.close()
